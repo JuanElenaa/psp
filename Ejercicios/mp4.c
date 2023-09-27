c415c98583a1cd8ad21c2cc9eecfc0d5e70302db
JuanElenaa <145117450+JuanElenaa@users.noreply.github.com>
2023-09-27 10:10:27 +0200

    Create mp4.c

---
+++ Ejercicios/mp4.c
@@ -0,0 +1,35 @@
+a)
+1000 (mp4)
+   |
+   └── 1001 (CCC)
+         |
+         ├── 1002 (???)
+         └── 1003 (???)
+
+     
+
+b) Podría producirse otra salida ya que el proceso hijo no espera al padre en ningún momento, por tanto la salida podría cambiar
+
+
+
+
+c) El código modificado para que salga CCC, BBB, AAA es:
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void main() {
+
+	printf("CCC \n");
+	
+	if (fork()!=0){
+		wait(NULL);
+ 		printf("AAA \n");
+ 		
+ 	}else{
+ 		printf("BBB \n");
+ 	}
+ 	
+ 	exit(0);
+}
