44523d4a0a4790369c0f511aee195099b828aa9d
JuanElenaa <145117450+JuanElenaa@users.noreply.github.com>
2023-10-30 17:13:38 +0100

    Update ipc5.c

--- UT1 - 04 - Ejercicios IPC - Unnamed Pipes/ipc5.c
+++ UT1 - 04 - Ejercicios IPC - Unnamed Pipes/ipc5.c
@@ -6,6 +6,9 @@
 int main() {
     int pipe1[2];
     int pipe2[2];
+    char letras[] = "TRWAGMYFPDXBNJZSQVHLCKE";
+    char letra[2];
+    char buffer[2];
     
     pipe(pipe1);
     pipe(pipe2);
@@ -13,34 +16,35 @@ int main() {
     pid_t pid = fork();
 
     if (pid == 0) { // Proceso hijo P2
-        close(pipe1[1]);
-        close(pipe2[0]);
+        close(pipe1[0]);
+        close(pipe2[1]);
 
         int dni;
         printf("Introduce el número de tu DNI: ");
         scanf("%d", &dni);
 
-        write(pipe1[0], &dni, sizeof(int));
+        write(pipe1[1], &dni, sizeof(int));
         
-        char letra;
-        read(pipe2[0], &letra, sizeof(char));
+        
+        read(pipe2[0], buffer, sizeof(char));
 
-        printf("La letra del NIF es %c\n", letra);
+        printf("La letra del NIF es %s\n", buffer);
 
     } else if (pid > 0) { // Proceso padre P1
-        close(pipe1[0]);
-        close(pipe2[1]);
+    	int dni;
+        close(pipe1[1]);
+        close(pipe2[0]);
 
-        int dni;
-        read(pipe1[1], &dni, sizeof(int));
-
-        char letras[] = "TRWAGMYFPDXBNJZSQVHLCKE";
-        char letra = letras[dni % 23];
         
-        write(pipe2[0], &letra, sizeof(char));
+        read(pipe1[0], &dni, sizeof(int));
+        
+        dni %= 23;
+
+        letra[0] = letras[dni];
+        
+        write(pipe2[1], &letra, sizeof(char));
 
         wait(NULL); // Espera a que el proceso hijo termine
     }
-
     return 0;
 }
