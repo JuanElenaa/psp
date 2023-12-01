6f3b9b82bd2091e3eab71254e30df71ba1f0b4f8
JuanElenaa <145117450+JuanElenaa@users.noreply.github.com>
2023-12-01 11:51:01 +0100

    Update signal1.c

--- UT1 - 06 - Ejercicios Señales/signal1.c
+++ UT1 - 06 - Ejercicios Señales/signal1.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
 
+// Función para manejar la señal SIGINT
+void sigint_handler(int signum) {
+
+    time_t t;
+    struct tm *tm;
+    char fechayhora[100];
+
+    t=time(NULL);
+    tm=localtime(&t);
+    strftime(fechayhora, 100, "%d/%m/%Y %H:%M:%S", tm);
+    
+    printf("Fin del proceso %d : %s\n", getpid(), fechayhora);
+    exit(EXIT_SUCCESS);
+}
+
+int main(void) {
+    signal(SIGINT, sigint_handler);
+
+    time_t t;
+    struct tm *tm;
+    char fechayhora[100];
+
+    t=time(NULL);
+    tm=localtime(&t);
+    strftime(fechayhora, 100, "%d/%m/%Y %H:%M:%S", tm);
+
+    printf("Inicio del proceso %d: %s\n",getpid(), fechayhora);
+
+    // Bucle infinito para mantener el programa en ejecución
+    while (1) {
+        // Haciendo que el programa espere por señales
+        sleep(1);
+    }
+
+    return 0;
+}
