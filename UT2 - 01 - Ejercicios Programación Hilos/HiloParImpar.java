572721e39e458b250de70da0216d663951230940
JuanElenaa <145117450+JuanElenaa@users.noreply.github.com>
2024-01-10 10:09:58 +0100

    Create HiloParImpar.java

---
+++ UT2 - 01 - Ejercicios Programación Hilos/HiloParImpar.java
@@ -0,0 +1,20 @@
+class HiloParImpar implements Runnable {
+    private int tipo;
+
+    public HiloParImpar(int tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public void run() {
+        if (tipo == 1) {
+            for (int i = 2; i <= 100; i = i + 2) {
+                System.out.println("HILO " + Thread.currentThread().getName() + " generando número par " + i);
+            }
+        } else if (tipo == 2) {
+            for (int i = 101; i <= 200; i = i + 2) {
+                System.out.println("HILO " + Thread.currentThread().getName() + " generando número impar " + i);
+            }
+        }
+    }
+}
